883dc6871f35016d976a7e17942f9446f79f6f66
Leonardo <Leonardotuio@hotmail.com>
2025-06-14 15:38:42 -0300

    Melhorias eventosDao! Melhoria em Crud aonde informações estavam incorretas

--- src/main/java/leonardo/barbosa/service/EventoService.java
+++ src/main/java/leonardo/barbosa/service/EventoService.java
@@ -1,4 +1,56 @@
 package leonardo.barbosa.service;
 
+import leonardo.barbosa.dao.AlunoDao;
+import leonardo.barbosa.dao.EventosDao;
+import leonardo.barbosa.model.Aluno;
+import leonardo.barbosa.model.Eventos;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventoService {
+
+    public boolean salvarBD(Eventos eventos){
+        var dao = new EventosDao();
+        return dao.salvar(eventos);
+    }
+
+    public List<Eventos> listarBD() {
+        List<Eventos> evento = new ArrayList<>();
+        var dao = new EventosDao();
+
+        dao.listar().forEach(object -> evento.add((Eventos) object));
+        return evento;
+    }
+
+    public boolean atualizarBD(Eventos eventos) {
+        var dao = new EventosDao();
+        return dao.atualizar(eventos);
+    }
+
+    public Boolean deletarEvento(Long id) {
+        if (id == null) return false;
+        var dao = new EventosDao();
+        return dao.deletar(id);
+    }
+
+    public String listar() {
+        var dao = new EventosDao();
+
+        StringBuilder result = new StringBuilder();
+        for (Object evento : dao.listar()) {
+            result.append("\n").append(evento);
+        }
+        return result.toString();
+    }
+
+    public List<Aluno> listarAlunoPorEvento(){
+        AlunoDao AlunoDao = new AlunoDao();
+        List<Aluno> alunos = new ArrayList<>();
+        AlunoDao.listar().forEach(obj -> alunos.add((Aluno)obj));
+        return alunos;
+    }
+
+
+
 }

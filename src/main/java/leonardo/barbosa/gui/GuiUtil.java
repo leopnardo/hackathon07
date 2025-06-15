ef84cfb921d3d27d0d9ef9b6e68d97bd5c5e0f30
Leonardo <Leonardotuio@hotmail.com>
2025-06-15 15:35:56 -0300

    Realinhamento do Código e limpeza

--- src/main/java/leonardo/barbosa/gui/GuiUtil.java
+++ src/main/java/leonardo/barbosa/gui/GuiUtil.java
@@ -6,12 +6,12 @@ public interface GuiUtil {
 
     default GridBagConstraints montarGrid(int x, int y, int w, int h) {
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx      = x;
-        constraints.gridy      = y;
-        constraints.gridwidth  = w;
+        constraints.gridx = x;
+        constraints.gridy = y;
+        constraints.gridwidth = w;
         constraints.gridheight = h;
-        constraints.insets     = new Insets(5, 5, 5, 5);
-        constraints.anchor     = GridBagConstraints.CENTER;
+        constraints.insets = new Insets(5, 5, 5, 5);
+        constraints.anchor = GridBagConstraints.CENTER;
         return constraints;
     }
 }

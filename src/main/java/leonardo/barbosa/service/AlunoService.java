dc6ff823d7a830423a27e7357b1165f7bab8e90b
Leonardo <Leonardotuio@hotmail.com>
2025-06-15 15:32:55 -0300

    Criação do metodo listar e ContarAlunosPorEvento e alinhamento de códigos.

--- src/main/java/leonardo/barbosa/service/AlunoService.java
+++ src/main/java/leonardo/barbosa/service/AlunoService.java
@@ -2,41 +2,30 @@ package leonardo.barbosa.service;
 
 import leonardo.barbosa.dao.AlunoDao;
 import leonardo.barbosa.model.Aluno;
+import leonardo.barbosa.model.AlunoEventos;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AlunoService {
 
-    public Boolean salvar(Aluno aluno){
-        var dao = new AlunoDao();
+    private final AlunoDao dao = new AlunoDao();
+
+    public Boolean Salvar(Aluno aluno) {
         return aluno.getId() == null
                 ? dao.salvar(aluno)
                 : dao.atualizar(aluno);
     }
 
-    public List<Aluno> listar(){
+    public List<Aluno> listar() {
         List<Aluno> alunos = new ArrayList<>();
-        var dao = new AlunoDao();
-
-
         dao.listar().forEach(object -> alunos.add((Aluno) object));
         return alunos;
     }
 
-    public String listarArquivos() {
-
-        var dao = new AlunoDao();
-
-        String result = "";
-        for (Object aluno : dao.listar()) {
-            result = result + "\n" + aluno;
-        }
-
-        return result;
+    public List<AlunoEventos> contarAlunosEvento() {
+        return dao.contarAlunosPorEvento();
     }
-
-
 
 
 }
